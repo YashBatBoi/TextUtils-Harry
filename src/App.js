7b38289f4f95fc4e0a1f8bc729e88caa002412ce
Yash Panchal <yashchiragpanchal@gmail.com>
2022-06-06 15:46:29 +0530

    Push to github

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-
+  
 function App() {
   return (
     <>
