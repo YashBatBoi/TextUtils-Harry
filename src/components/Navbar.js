2409b2599af8e63037b59a07a577dbb327ff4ccc
YashBatBoi <yashchiragpanchal@gmail.com>
2022-08-02 12:27:14 +0530

    Deploying Site

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 // this is also call a function based component ~function Navbar()~
 export default function Navbar(props) {
   return (
     <nav
-      className={`navbar navbar-expand-lg navbar-${
-        props.mode === "danger" ? "light" : props.mode
-      } bg-${props.mode}`}
+      className={`navbar navbar-expand-lg navbar-${props.mode === "danger" ? "light" : props.mode
+        } bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <Link
-          className={`navbar-brand text-${
-            props.mode === "danger"
+        <a
+          className={`navbar-brand text-${props.mode === "danger"
               ? "light"
               : "dark" && props.mode === "dark"
-              ? "light"
-              : "dark"
-          }`}
-          to="/home"
+                ? "light"
+                : "dark"
+            }`}
+          href="#"
         >
           {props.title}
-        </Link>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -37,21 +35,20 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link
-                to="/"
-                className={`nav-link active text-${
-                  props.mode === "danger"
+              <a
+                href="#"
+                className={`nav-link active text-${props.mode === "danger"
                     ? "light"
                     : "dark" && props.mode === "dark"
-                    ? "light"
-                    : "dark"
-                }`}
+                      ? "light"
+                      : "dark"
+                  }`}
                 aria-current="page"
               >
                 Home
-              </Link>
+              </a>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link
                 to="/about"
                 className={`nav-link text-${
@@ -64,7 +61,7 @@ export default function Navbar(props) {
               >
                 {props.about}
               </Link>
-            </li>
+            </li> */}
           </ul>
           {/* <form className="d-flex">
             <input
@@ -78,13 +75,12 @@ export default function Navbar(props) {
             </button>
           </form> */}
           <div
-            className={`form-check form-switch text-${
-              props.mode === "dark"
+            className={`form-check form-switch text-${props.mode === "dark"
                 ? "light"
                 : "dark" && props.mode === "danger"
-                ? "light"
-                : "dark"
-            } mx-3`}
+                  ? "light"
+                  : "dark"
+              } mx-3`}
           >
             <input
               className="form-check-input"
@@ -102,13 +98,12 @@ export default function Navbar(props) {
           </div>
 
           <div
-            className={`form-check form-switch text-${
-              props.mode === "dark"
+            className={`form-check form-switch text-${props.mode === "dark"
                 ? "light"
                 : "dark" && props.mode === "danger"
-                ? "light"
-                : "dark"
-            }`}
+                  ? "light"
+                  : "dark"
+              }`}
           >
             <input
               className="form-check-input"
